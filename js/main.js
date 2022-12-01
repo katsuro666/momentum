fdbffa0cfba342f0d73cdd168ceb7452737edea5
katsuro666 <katsuro666@Macbooks-MacBook-Pro.local>
2022-12-01 17:50:38 +0300

    feat: add slider

--- js/main.js
+++ js/main.js
@@ -26,21 +26,21 @@ function showDate() {
 
 /// ========= Greeting section
 const userGreeting = document.querySelector('.greeting')
+const currentHour = newDate.getHours();
+
+function getTimeOfDay(hour) {
+  if (hour >= 0 && hour < 6) {
+    return 'night'
+  } else if (hour >= 6 && hour < 12) {
+    return 'morning'
+  } else if (hour >= 12 && hour < 18) {
+    return 'afternoon'
+  } else {
+    return 'evening'
+  } 
+}
 
 function showGreeting() {
-  const currentHour = newDate.getHours();
-
-  function getTimeOfDay(hour) {
-    if (hour >= 0 && hour < 6) {
-      return 'night'
-    } else if (hour >= 6 && hour < 12) {
-      return 'morning'
-    } else if (hour >= 12 && hour < 18) {
-      return 'afternoon'
-    } else {
-      return 'evening'
-    } 
-  }
   const greetingText = `Good ${getTimeOfDay(currentHour)}`
   return userGreeting.textContent = greetingText
 }
@@ -55,6 +55,50 @@ function showTime() {
   showDate()
   showGreeting()
   setTimeout(showTime, 1000)
-  }
+}
   
-  showTime()
+showTime()
+
+/// ========= Slider section
+const slideNext = document.querySelector('.slide-next')
+const slidePrev = document.querySelector('.slide-prev')
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let randomNum = getRandomInt(10, 20)
+
+function getSlideNext() {
+  if (randomNum < 20) {
+    randomNum += 1
+  } else if (randomNum == 20) {
+    randomNum = 10
+  }
+  setBg()
+}
+function getSlidePrev() {
+  if (randomNum > 10) {
+    randomNum -= 1
+  } else if (randomNum == 10) {
+    randomNum = 20
+  }
+  setBg()
+}
+
+slideNext.addEventListener('click', getSlideNext)
+slidePrev.addEventListener('click', getSlidePrev)
+
+function setBg() {
+  let bgNum = randomNum
+  let timeOfDay = getTimeOfDay(currentHour)
+  let img = new Image();
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`
+  img.onload = () => {
+    document.body.style.backgroundImage = `url(https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg)`
+  }
+}
+
+setBg()
