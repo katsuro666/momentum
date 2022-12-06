28ea4c180c9ba6f325306258b2d17af1ad8b4ca1
katsuro666 <katsuro666@Macbooks-MacBook-Pro.local>
2022-12-06 10:57:51 +0300

    feat: add support for English and Spanish languages

--- js/main.js
+++ js/main.js
@@ -16,6 +16,64 @@ function getLocalStorage() {
 }
 window.addEventListener('load', getLocalStorage)
 
+/// ========= Add other languages section
+
+const dropdownMenu = document.querySelector('.dropdown__content')
+// const dropdownLang = document.querySelectorAll('.dropdown-language')
+const currentLang = document.querySelector('.current-language')
+// const hideLang = document.querySelector('.language-hidden')
+
+const langEnglish = document.querySelector('.lang-en')
+const langRussian = document.querySelector('.lang-ru')
+const langSpanish = document.querySelector('.lang-es')
+
+let pageLang = 'en'
+currentLang.textContent = langEnglish.textContent
+
+function dropdownToggle() {
+  dropdownMenu.classList.toggle('dropdown--hidden')
+}
+currentLang.addEventListener('click', dropdownToggle)
+
+
+langRussian.addEventListener('click', function() {
+  currentLang.textContent = langRussian.textContent
+  langEnglish.classList.remove('language-hidden')
+  langSpanish.classList.remove('language-hidden')
+  langRussian.classList.add('language-hidden')
+  pageLang = 'ru'
+  dropdownToggle()
+  getWeather()
+  newQuote()
+})
+langSpanish.addEventListener('click', function() {
+  currentLang.textContent = langSpanish.textContent
+  langEnglish.classList.remove('language-hidden')
+  langRussian.classList.remove('language-hidden')
+  langSpanish.classList.add('language-hidden')
+  pageLang = 'es'
+  dropdownToggle()
+  getWeather()
+  newQuote()
+})
+langEnglish.addEventListener('click', function() {
+  currentLang.textContent = langEnglish.textContent
+  langSpanish.classList.remove('language-hidden')
+  langRussian.classList.remove('language-hidden')
+  langEnglish.classList.add('language-hidden')
+  pageLang = 'en'
+  dropdownToggle()
+  getWeather()
+  newQuote()
+})
+
+document.addEventListener('click', e => {
+  if (e.target !== dropdownMenu && e.target !== currentLang) {
+    dropdownMenu.classList.add('dropdown--hidden')
+    // console.log(e.target)
+  }
+});
+
 /// ========= Date section
 
 const date = document.querySelector('.date')
@@ -28,8 +86,16 @@ function currentHour() {
 function showDate() {
   newDate = new Date()
   let options = { weekday: 'long', month: 'long', day: 'numeric' };
-  let currentDate = newDate.toLocaleDateString('en-US', options)
-  date.textContent = currentDate
+  if (pageLang === 'en') {
+    let currentDate = newDate.toLocaleDateString('en-US', options)
+    date.textContent = currentDate
+  } else if (pageLang === 'ru') {
+    let currentDate = newDate.toLocaleDateString('ru-RU', options)
+    date.textContent = currentDate
+  } else if (pageLang === 'es') {
+    let currentDate = newDate.toLocaleDateString('es-ES', options)
+    date.textContent = currentDate
+  }
 }
 
 /// ========= Greeting section
@@ -48,9 +114,48 @@ function getTimeOfDay(hour) {
   } 
 }
 
-function showGreeting() {
-  const greetingText = `Good ${getTimeOfDay(currentHour())}`
-  return userGreeting.textContent = greetingText
+function getTimeOfDayRU(hour) {
+  if (hour >= 0 && hour < 6) {
+    return 'Доброй ночи'
+  } else if (hour >= 6 && hour < 12) {
+    return 'Доброе утро'
+  } else if (hour >= 12 && hour < 18) {
+    return 'Добрый день'
+  } else {
+    return 'Добрый вечер'
+  } 
+}
+
+function getTimeOfDayES(hour) {
+  if (hour >= 0 && hour < 6) {
+    return 'Buenas noches'
+  } else if (hour >= 6 && hour < 12) {
+    return 'Buenos dias'
+  } else if (hour >= 12 && hour < 18) {
+    return 'Buenas tardes'
+  } else {
+    return 'Buenas noches'
+  } 
+}
+
+const greetingTranslation = {
+  'English': `Good ${getTimeOfDay(currentHour())},`,
+  'Russian': `${getTimeOfDayRU(currentHour)},`,
+  'Spanish': `${getTimeOfDayES(currentHour)},`,
+}
+
+// function showGreeting(lang) {
+//   userGreeting.textContent = greetingTranslation[`${lang}`]
+// }
+
+function showaGreeting() {
+  if (pageLang === 'en') {
+    userGreeting.textContent = greetingTranslation['English']
+  } else if (pageLang === 'ru') {
+    userGreeting.textContent = greetingTranslation['Russian']
+  } else if (pageLang === 'es') {
+    userGreeting.textContent = greetingTranslation['Spanish']
+  }
 }
 
 /// ========= Time section
@@ -62,7 +167,7 @@ function showTime() {
   let currentTime = newDate.toLocaleTimeString()
   time.textContent = currentTime
   showDate()
-  showGreeting()
+  showaGreeting()
   setTimeout(showTime, 1000)
 }
   
@@ -120,11 +225,11 @@ const weatherIcon = document.querySelector('.weather-icon')
 const temperature = document.querySelector('.temperature')
 const weatherDescription = document.querySelector('.weather-description')
 
+userCity.value = 'Moscow'
 userCity.addEventListener('change', getWeather)
 
-
 async function getWeather() {
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${userCity.value}&lang=en&appid=08983b23d3ae989b57d8a4f5432db702&units=metric`
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${userCity.value}&lang=${pageLang}&appid=08983b23d3ae989b57d8a4f5432db702&units=metric`
   let res = await fetch(url)
   let data = await res.json()
   weatherIcon.className = 'weather-icon owf'
@@ -141,17 +246,43 @@ const quote = document.querySelector('.quote')
 const quoteAuthor = document.querySelector('.author')
 const changeQuote = document.querySelector('.change-quote')
 
-changeQuote.addEventListener('click', getQuotes)
+function newQuote() {
+  if (pageLang === 'en') {
+    getQuotesEN()
+  } else if (pageLang === 'ru') {
+    getQuotesRU()
+  } else if (pageLang === 'es') {
+    getQuotesES()
+  }
+}
 
-async function getQuotes() {
-  let quotes = 'js/quotes.json'
+changeQuote.addEventListener('click', newQuote)
+
+async function getQuotesEN() {
+  let quotes = 'js/quotesEN.json'
   let res = await fetch(quotes)
   let data =  await res.json()
   let randomNumber = getRandomInt(0, 9)
   quote.textContent = data[randomNumber].text
   quoteAuthor.textContent = data[randomNumber].author
 }
-getQuotes()
+async function getQuotesRU() {
+  let quotes = 'js/quotesRU.json'
+  let res = await fetch(quotes)
+  let data =  await res.json()
+  let randomNumber = getRandomInt(0, 9)
+  quote.textContent = data[randomNumber].text
+  quoteAuthor.textContent = data[randomNumber].author
+}
+async function getQuotesES() {
+  let quotes = 'js/quotesES.json'
+  let res = await fetch(quotes)
+  let data =  await res.json()
+  let randomNumber = getRandomInt(0, 9)
+  quote.textContent = data[randomNumber].text
+  quoteAuthor.textContent = data[randomNumber].author
+}
+getQuotesEN()
 
 /// ========= Audio player section
 
